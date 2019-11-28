2b301eafbc6ad31c1cab4084f914a43d0d217d9f
Ryogo Waki <rw1860@docon.jp>
2019-11-28 15:19:49 +0900

    CalcPage.xaml, CalcPageVM.cs作成

---
+++ DamageCalculator/ViewModel/CalcPageVM.cs
@@ -0,0 +1,13 @@
+﻿//using
+
+namespace DamageCalculator.ViewModel
+{
+    public class CalcPageVM
+    {
+        private double Calc()
+        {
+            double result = 0;
+            return result;
+        }
+    }
+}
