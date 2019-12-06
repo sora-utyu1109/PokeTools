58988f67e8f25136004ce3016a71fdffad041b32
Ryogo Waki <rw1860@docon.jp>
2019-12-06 10:55:26 +0900

    NotificationBase追加

---
+++ DamageCalculator/Util/NotificationBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DamageCalculator.Util
+{
+    public class NotificationBase : INotifyPropertyChanged
+    {
+        protected void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) { return false; }
+            field = value;
+            this.RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
