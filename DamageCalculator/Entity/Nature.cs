373a547bc0415478edb4b334b5f0d43518048571
Ryogo Waki <rw1860@docon.jp>
2019-12-10 10:06:17 +0900

    Entity追加

---
+++ DamageCalculator/Entity/Nature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DamageCalculator.Entity
+{
+    public class Nature
+    {
+        public string Name { get; set; }
+        public string Up { get; set; }
+        public string Down { get; set; }
+        public override string ToString()
+        {
+            return String.Format("{0}↑{1}↓", Up, Down);
+        }
+    }
+}
