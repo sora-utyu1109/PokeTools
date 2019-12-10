cee1da06b49c9df6f2efba9d838da157896be892
Ryogo Waki <rw1860@docon.jp>
2019-12-10 10:07:58 +0900

    その他修正

---
+++ DamageCalculator/ConstDef.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DamageCalculator
+{
+    public class ConstDef
+    {
+        public const int LV50 = 50;
+        public const int LV100 = 100;
+        public const int IV = 31;
+        public const int EV = 0;
+        public const string NAME = "NAME";
+        public const string TYPES = "TYPES";
+        public const string KG = "KG";
+        public const string H = "H";
+        public const string A = "A";
+        public const string B = "B";
+        public const string C = "C";
+        public const string D = "D";
+        public const string S = "S";
+    }
+}
